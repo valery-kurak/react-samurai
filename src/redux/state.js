e0be476ed564237089918dbf9d200cdc6124d7de
Valery Kurak <valerakurak344@gmail.com>
2022-12-24 13:25:28 +0300

    Completed lesson 33

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import { rerenderEntireTree } from "../render";
+
 let state = {
     profilePage: {
         posts: [
@@ -27,6 +29,7 @@ export let addPost = (postMessage) => {
     };
 
     state.profilePage.posts.push(newPost);
+    rerenderEntireTree(state);
 }
 
 export default state;
