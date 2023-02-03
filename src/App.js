988f4eb56416e38435a45e03742da8abbda24c5d
Valery Kurak <valerakurak344@gmail.com>
2023-02-04 00:12:09 +0300

    Completed lesson 43

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import {Route, Routes} from "react-router-dom";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 const App = (props) => {
 
@@ -18,11 +18,9 @@ const App = (props) => {
         <div className='app-wrapper-content'>
           <Routes>
             <Route path='dialogs/*' 
-                   element={ <Dialogs store={props.store}/> }/>
+                   element={ <DialogsContainer store={props.store}/> }/>
             <Route path='profile/*' 
-                   element={ <Profile 
-                     profilePage={props.state.profilePage}
-                     dispatch={props.dispatch}/>
+                   element={ <Profile store={props.store}/>
                    }/>
             <Route path='news/*' 
                    element={ <News /> }/>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,19 +5,19 @@ import Message from "./Message/Message";
 import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-reducer";
 
 const Dialogs = (props) => {
-  let state = props.store.getState().dialogsPage;
+  let state = props.dialogsPage;
 
   let dialogsElements = state.dialogs.map( d => <DialogItem id={d.id} name={d.name} /> );
   let messagesElements = state.messages.map( m => <Message message={m.message} /> );
   let newMessageBody = state.newMessageBody;
 
   let onSendMessageClick = () => {
-    props.store.dispatch(sendMessageCreator());
+    props.sendMessage();
   }
 
   let onNewMessageChange = (e) => {
     let body = e.target.value;
-    props.store.dispatch(updateNewMessageBodyCreator(body));
+    props.updateNewMessageBody(body);
   }
 
   return (
