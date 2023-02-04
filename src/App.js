598acfeb584572dad0f7c8bbc9aafeb6208fcaa9
Valery Kurak <valerakurak344@gmail.com>
2023-02-04 22:41:18 +0300

    Completed lesson 44

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Profile from './components/Profile/Profile';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 const App = (props) => {
@@ -18,9 +18,9 @@ const App = (props) => {
         <div className='app-wrapper-content'>
           <Routes>
             <Route path='dialogs/*' 
-                   element={ <DialogsContainer store={props.store}/> }/>
+                   element={ <DialogsContainer /> }/>
             <Route path='profile/*' 
-                   element={ <Profile store={props.store}/>
+                   element={ <Profile />
                    }/>
             <Route path='news/*' 
                    element={ <News /> }/>
--- src/index.js
+++ src/index.js
@@ -4,24 +4,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "./StoreContext";
 
-let rerenderEntireTree = (state) => {
+let rerenderEntireTree = () => {
   ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App 
-          state={state} 
-          dispatch={store.dispatch.bind(store)}
-          store={store}/>
-      </BrowserRouter>
-    </React.StrictMode>,
+      <React.StrictMode>
+          <BrowserRouter>
+              <Provider store={store}>
+                  <App/>
+              </Provider>
+          </BrowserRouter>
+      </React.StrictMode>,
     document.getElementById('root')
   );
 }
 
-rerenderEntireTree(store.getState());
+rerenderEntireTree();
 
 store.subscribe(() => {
-    let state = store.getState();
-    rerenderEntireTree(state);
+    rerenderEntireTree();
 });
