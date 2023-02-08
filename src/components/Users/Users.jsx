cfd0165cc031bbf211b94f7956eb0c700a22d10f
Valery Kurak <valerakurak344@gmail.com>
2023-02-08 22:30:23 +0300

    Completed lessons 50-51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import styles from "./users.module.css";
+import axios from "axios";
+import userPhoto from "../../assets/images/user-avatar.png"
 
 let Users = (props) => {
-    if (props.users.length === 0) { 
-        props.setUsers([
-            {id: 1, photoUrl: 'https://www.pngitem.com/pimgs/m/560-5603874_product-image-logo-avatar-minimalist-flat-line-hd.png',
-                followed: false, fullName: 'Valery1', status: 'status1', location: {country: 'Belarus', city: 'Minsk'} },
-            {id: 2, photoUrl: 'https://www.pngitem.com/pimgs/m/560-5603874_product-image-logo-avatar-minimalist-flat-line-hd.png',
-                followed: true, fullName: 'Valery2', status: 'status2', location: {country: 'Ukraine', city: 'Kyiv'} },
-            {id: 3, photoUrl: 'https://www.pngitem.com/pimgs/m/560-5603874_product-image-logo-avatar-minimalist-flat-line-hd.png',
-                followed: false, fullName: 'Valery3', status: 'status3', location: {country: 'Poland', city: 'Warsaw'} }
-        ]);
+    if (props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => {
+            props.setUsers(response.data.items);
+        });
     }
-
 
     return <div>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto} alt='avatar' />
+                        <img
+                            src={ u.photos.small !=null ? u.photos.small : userPhoto}
+                            className={styles.userPhoto} alt='avatar' />
                     </div>
                     <div>
                         { u.followed
@@ -29,12 +28,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 
 ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-          <Provider store={store}>
-              <App/>
-          </Provider>
-      </BrowserRouter>
-    </React.StrictMode>, document.getElementById('root'));
+    <BrowserRouter>
+      <Provider store={store}>
+          <App/>
+      </Provider>
+    </BrowserRouter>, document.getElementById('root'));
