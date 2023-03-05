8c33f127cafc30ca0eb407124533bb7a17e7854b
Valery Kurak <valerakurak344@gmail.com>
2023-03-05 23:12:07 +0300

    Completed lesson 78

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,8 @@ const Header = (props) => {
         <header className={s.header}>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png" alt="logo"/>
                 <div className={s.loginBlock}>
-                    { props.isAuth ? props.login
+                    { props.isAuth
+                        ? <div>{props.login} - <button onClick={props.logout}>Log out</button></div>
                         : <NavLink to={'/login'}>Login</NavLink>
                     }
                 </div>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,26 +2,33 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 import { Input } from "../common/FormsControls/FormsControls";
 import { required } from "../../utils/validators/validators";
+import { connect } from "react-redux";
+import { login } from "../../redux/auth-reducer";
+import {Navigate} from "react-router-dom";
 
 const Login = (props) => {
     const onSubmit = (formData) => {
-        console.log(formData);
+        props.login(formData.email, formData.password, formData.rememberMe);
+    };
+
+    if (props.isAuth) {
+        return <Navigate to={"/profile"} />
     }
 
     return <div>
         <h1>Login</h1>
         <LoginReduxForm onSubmit={onSubmit} />
     </div>
-}
+};
 
 const LoginForm = (props) => {
     return <form onSubmit={props.handleSubmit}>
         <div>
-            <Field placeholder={"Login"} name={"login"}
+            <Field placeholder={"Email"} name={"email"}
                    component={Input} validate={[required]} />
         </div>
         <div>
-            <Field placeholder={"Password"} name={"password"}
+            <Field placeholder={"Password"} name={"password"} type={"password"}
                    component={Input} validate={[required]} />
         </div>
         <div>
@@ -32,8 +39,12 @@ const LoginForm = (props) => {
             <button>Login</button>
         </div>
     </form>
-}
+};
 
 const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm);
 
-export default Login;
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth
+})
+
+export default connect(mapStateToProps, { login })(Login);
