3a06019a91143a300e7b946b53aee2a5a394ceff
Valery Kurak <valerakurak344@gmail.com>
2023-04-02 21:39:59 +0300

    Completed lesson 93

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,7 +6,7 @@ let Users = ({currentPage, totalUsersCount, pageSize, onPageChanged, users, ...p
     return <div>
         {
             <Paginator currentPage={currentPage}
-                       totalUsersCount={totalUsersCount}
+                       totalItemsCount={totalUsersCount}
                        pageSize={pageSize}
                        onPageChanged={onPageChanged} />
         }
